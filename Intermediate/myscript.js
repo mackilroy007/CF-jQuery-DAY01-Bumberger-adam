258fa23f0431aaecf859f0c1907d7fbe823c776d
AdamAl-Salehi <email@example.combakr.arkan@icloud.com>
2020-10-05 13:39:59 +0200

    intermediate done

--- Intermediate/myscript.js
+++ Intermediate/myscript.js
@@ -9,7 +9,7 @@ $("form").submit(function(e) {
 //rmove the form element
 /*$("form").remove()*/
 
-//add 5 new divs in the article element
+//add 5 new divs in the article element 
 $(document).ready(function() {
     var divi;
     for (var i = 0; i < 5; i++) {
@@ -20,5 +20,15 @@ $(document).ready(function() {
 //3.
 $(document).ready(function(){
 	$("input").val("Search for");
+});
+//4. it can be put inside the function, inside the document ready or here but it has to be in a document ready
 
+$(document).ready(function(){
+	$("div").addClass("box");
+});
+//5.
+$(document).ready(function(){
+
+	$("a[href ='http://www.codefactory.com/']").attr("href","www.codefactory.wien");
+	//$("a").attr("href", "www.codefactory.wien")
 });
